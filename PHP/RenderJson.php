9f5cd008bfba4789c8f114f5076cf929090abe03
codestk <codestk@gmail.com>
2017-04-15 12:02:29 +0700

    oi9oik

--- PHP/RenderJson.php
+++ PHP/RenderJson.php
@@ -1,6 +1,6 @@
 <?php
-phpinfo();
-include 'db.php';
+ 
+include 'FrameWork/DataBase/db.php';
 //post_status=all&post_type=nf_sub&form_id=3
 $sql = " SELECT * FROM test WHERE 1";
 $result = $conn->query($sql);
@@ -9,14 +9,11 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-
 		$rows[] = $row ;
     }
 } else {
     echo "0 results";
 }
-
 print json_encode($rows);
 $conn->close();
-
 ?>
